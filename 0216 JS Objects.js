b3340052d7fa06604ddb34f9fc281f3d19ac76b8
adampi <adam.piskorek.pl@gmail.com>
2018-02-16 18:28:19 +0100

    Add files via upload

---
+++ 0216 JS Objects.js
@@ -0,0 +1,11 @@
+// Objekty
+
+'use strict';
+
+var cat = {name: "Fluffy", color: "White"};
+cat.age = 3
+//display(cat.age)
+cat.speak = function() { display("Meeoooow")}
+cat.speak()
+
+///////////////////////////////////////
