46dd4db884b80d57746b678d090d538898e1e89a
hvitis <adam.piskorek.pl@gmail.com>
2018-02-27 22:56:55 +0100

    Binding and last steps of higher order functions

--- 0225 JSON.js
+++ 0225 JSON.js
@@ -115,3 +115,44 @@ function female(p) { return p.sex == "f"; }
 
 console.log(average(ancestry.filter(male).map(age)))
 console.log(average(ancestry.filter(female).map(age)))
+
+// Object that associates names with people
+/*
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+console.log(byName["Philibert Haverbeke"])
+
+function reduceAncestors(person, f, defaultValue) {
+  function valueFor(person) {
+    if (person == null)
+     return defaultValue;
+    else
+     return f(person, valueFor(byName[person.mother]),
+                      valueFor(byName[person.father])); 
+  }
+  return valueFor(person);
+}
+
+function sharedDNA(person, fromMother, fromFather) {
+  if (person.name = "Pauwels van Haverbeke")
+    return 1;
+  else
+    return (fromMother + fromFather) / 2;
+}
+
+var ph = byName["Philibert Haverbeke"];
+console.log(reduceAncestors(ph, sharedDNA, 0) / 4);
+*/
+
+// Binding
+var theSet = ["Carel Haverbeke", "Maria van Brussel", "Donald Duck"];
+function isInSet(set, person) {
+  return set.indexOf(person.name) > -1;
+}
+
+console.log(ancestry.filter(function(person){
+  return isInSet(theSet, person)
+}));
+
