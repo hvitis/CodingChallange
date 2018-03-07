f61d21cec5c375c813507adbcac4e30cbe9b697c
Piskorek <adam.piskorek.pl@gmail.com>
2018-03-06 21:57:32 -1200

    Project moving forward

--- 0306 Electronic life/0306 Representing space.js
+++ 0306 Electronic life/0306 Representing space.js
@@ -1,3 +1,5 @@
+// ELECTRONIC LIFE PROJECT
+
 function Vector(x, y) {
     this.x = x;
     this.y = y;
@@ -6,3 +8,28 @@ Vector.prototype.plus = function(other) {
     return new Vector(this.x + other.x, this.y + other.y);
 };
 
+var grid = [["top left", "top middle", "top right"], ["bottom left", "bottom middle", "bottom right"]]
+console.log(grid[1][1]);
+var grid = ["top left", "top middle", "top right", "bottom left", "bottom middle", "bottom right"]
+/* 
+Or we can use a single array, with size width * height, and decide that
+the element at (x,y) is found at position x + (y * width) in the array.
+*/
+console.log(grid[2 + (1*3)]) // bottom right
+console.log(grid[0 + (0*3)]) // top left
+console.log(grid[1 + (0*3)]) // top mid
+console.log(grid[2 + (0*3)]) // top right
+
+function Grid(width, height) {
+    this.space = new Array(width*height);
+    this.width = width;
+    this.height = height;
+}
+// console.log(new Grid(5,4));
+Grid.prototype.isInside = function(vector) {
+    return vector.x >= 0 && vector.x < this.width &&
+           vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector, value) {
+    return this.space[vector.x + this.width * vector.y];
+};
