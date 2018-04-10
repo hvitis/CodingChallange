baf5be3ded46360d3848f774c0e1538d583f938b
hvitis <adam.piskorek.pl@gmail.com>
2018-04-10 23:36:52 +0200

    Color blinking

--- 03 March/0321 Udemy/0409 Events/functions.js
+++ 03 March/0321 Udemy/0409 Events/functions.js
@@ -1,15 +1,17 @@
-var red = document.body.style.background = "red"
-document.querySelector("button").addEventListener("click", function() {
-    if (document.body.style.background == "red none repeat scroll 0% 0%")
+var black = document.body.style.background = "black"
+document.querySelector(".btn-primary").addEventListener("click", function() {
+    if (document.body.style.background == "black none repeat scroll 0% 0%")
     {
-        document.body.style.background = "blue none repeat scroll 0% 0%"
+        document.body.style.background = "white none repeat scroll 0% 0%"
     } 
     else 
     {
-        document.body.style.background = "red none repeat scroll 0% 0%"  
+        document.body.style.background = "black none repeat scroll 0% 0%"  
     }
 })
 
+
+/*
 // var green = document.querySelector(".btn-primary").style.background = "red"
 document.querySelector(".btn-primary").addEventListener("click", function() {
     if(document.querySelector(".btn-primary").style.background != "red"){
@@ -17,3 +19,4 @@ document.querySelector(".btn-primary").addEventListener("click", function() {
     }})
 
     // Why its working only with the first button??
+*/
