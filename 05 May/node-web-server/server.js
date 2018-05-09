a1632006065ca6de5c84e346520b8f3a9270e676
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2018-05-09 15:16:07 +0200

    Update server.js
    Tips in comments

--- 05 May/node-web-server/server.js
+++ 05 May/node-web-server/server.js
@@ -3,35 +3,43 @@ const fs = require("fs");
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    var log = "Now is " + now + req.method + req.url;
-    console.log(log);
-    fs.appendFile('server.log', log + "\r\n", (err) => {
-        if (err) {
-            console.log("Was unable to do that!");
-        }
-    });
-    next();
 
-}); 
-
-
-
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
 //    res.send("<h1>hello Express!</h1>");
-    res.send({
+    res.locals.statusCode = 200;
+    res.locals.data = {
         name: "Adam",
         wzrost: 200,
         oczy: [
             "jedno",
             "drugie"
         ]
-    })
+    };
+    next();
 });
 
-app.get("/about", (req, res) => {
-    res.send("About")}
-);
+app.get("/about", (req, res, next) => {
+    res.locals.statusCode = 200;
+    res.locals.data = "about";
+    next();
+});
+
+
+// You can use a Middleware to handle the responses, 
+// this middleware, because it was set after the routes, 
+// it will be called after them (if the routes use next()),
+// in this way you have the status answer, the data you are sending
+// and even more important, you only have one point where you send the answers,
+// so it is very easy to for example add additional headers.
+app.use((req, res, next) => {
+    res.status(res.locals.statusCode).send(res.locals.data);
+    // You should start learning the new javascript things - http://www.jstips.co/en/javascript/keyword-var-vs-let/
+    // new ways of declaring vars, "let" and "const"
+    const now = new Date().toString();
+    // Also, "template strings" using ``
+    const log = `${now} [${req.method}] ${req.url} - ${res.locals.statusCode}:
+    ${res.locals.data}`;
+    console.log(log);
+}); 
 
 app.listen(3000);
