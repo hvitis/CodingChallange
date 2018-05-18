bd14caa5e02007333ddde95bd92fe7e4ff40dafa
hvitis <adam.piskorek.pl@gmail.com>
2018-05-19 00:25:28 +0200

    phaser

---
+++ 18 May/VoiceGame/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+const port = 3000
+
+// app.use(express.static('js'))
+app.use('/js', express.static(path.join(__dirname, 'js')))
+//  app.use(express.static('index.html"))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
---
+++ 18 May/VoiceGame/js/main.js
@@ -0,0 +1,19 @@
+
+var GameState = {
+    preload: function() {
+
+    },
+    create: function(){
+
+    },
+    update: function() {
+
+    },
+
+}
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+game.state.add("GameState", GameState);
+
+game.state.start("GameState");
